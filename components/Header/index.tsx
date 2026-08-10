5ae209b6a25625297fa2ea89f6cc2d3ef0e0d79b
Oscar Öjling <oscarojling@gmail.com>
2026-08-10 12:01:33 +0200

    Added header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex justify-center">
+      <h1 className="text-2xl">Guess the player</h1>
+    </header>
+  )
+}
+
+export default Header
