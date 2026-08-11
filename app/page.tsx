a9dda2120e97cfa416e2738f299e52debd349f00
Oscar Öjling <oscarojling@gmail.com>
2026-08-11 14:37:42 +0200

    Reveal works

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
+import Game from "@/components/Game";
 import Header from "@/components/Header";
-import SelectPlayer from "@/components/SelectPlayer";
-import { players } from "@/data/players";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div>
-        {players.map((player) => (
-          <SelectPlayer key={player.name} player={player} />
-        ))}
-      </div>
+      <Game />
     </>
   );
 }
--- components/Game/index.tsx
+++ components/Game/index.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
-import { players } from "@/data/players"
-import Flashcard from "../Flashcard"
+"use client";
+
+import { useState } from "react";
+import { players } from "@/data/players";
+import Flashcard from "../Flashcard";
 
 const Game = () => {
-  const [currentindex, setCurrentIndex] = useState<number>(0)
-  const [isShown, setIsShown] = useState<boolean>(false)
-
+  const [currentindex, setCurrentIndex] = useState<number>(0);
+  const [isShown, setIsShown] = useState<boolean>(false);
 
   const handleNext = () => {
-    return (
-      currentindex = 
-    )
-  }
-}
+    setCurrentIndex(currentindex + 1);
+    setIsShown(false);
+  };
+  return (
+    <div>
+      <Flashcard
+        player={players[currentindex]}
+        isShown={isShown}
+        onReveal={() => setIsShown(true)}
+      />
+      {isShown && <button onClick={handleNext}>Next</button>}
+    </div>
+  );
+};
+
+export default Game;
