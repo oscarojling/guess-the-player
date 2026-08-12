df142ccef417b959ab8dbe3ed8d7aa3ff308b029
Oscar Öjling <oscarojling@gmail.com>
2026-08-12 12:52:07 +0200

    Added subtitle and test for header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,10 @@
+import SubTitle from "../SubTitle"
+
 const Header = () => {
   return (
-    <header className="flex justify-center">
+    <header className="flex text-center justify-center flex-col">
       <h1 className="text-2xl">Guess the player</h1>
+      <SubTitle text="Sweden World Cup 2026" />
     </header>
   )
 }
---
+++ components/SubTitle/index.tsx
@@ -0,0 +1,5 @@
+const SubTitle = ({ text }: { text: string }) => {
+  return <h2 className="text-black text-center m-4">{text}</h2>;
+};
+
+export default SubTitle;
